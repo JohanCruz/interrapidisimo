c900894ba8a3d7a88cdf8f4142f510eccfeb1b1f
JohanCruz <johandanielcruz@gmail.com>
2025-04-27 20:38:56 -0500

    enabled connection with .env

--- src/typeorm/data-source.ts
+++ src/typeorm/data-source.ts
@@ -1,18 +1,15 @@
 import { DataSource } from "typeorm";
 import * as dotenv from "dotenv";
 
+dotenv.config();
 
 export const dataSource = new DataSource({
-  type: "mysql",
-  port: 3306,
-  username: "root",
-  password: "",
-  database: "uni",
-  //port: parseInt(process.env.DB_PORT || "3306"), // Default 3306
-  //username: process.env.DB_USER || "root",
-  //password: process.env.DB_PASSWORD || "",
-  //database: process.env.DB_NAME || "uni",
-  
+  type: "mysql", 
+  port: parseInt(process.env.DB_PORT || "3306"), // Default 3306
+  username: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "uni",
+    
   synchronize: true,
   logging: false, // <--- Esto oculta las queries
   migrations: [],
